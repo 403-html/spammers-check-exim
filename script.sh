8561903641192b4b4110ebbdfd2116d0e44faaf5
WhiteLeash <tymoteusz.stepien@wp.pl>
2019-01-15 07:17:31 +0100

    Added functions than plain script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# version 1.0.0
+# version 1.0.1
 #
 
 # Variables
@@ -9,33 +9,51 @@ MAIL_LOG="/var/log/exim_mainlog"
 SUBJECT="Spam check on $HOSTNAME"
 MAIL_DESTINATION="example@example.com"
 BODY_FILE="msg-body.txt"
+NUMER_OF_PEOPLE=5
 
-# Main Script
+# Script
 
-if [ -f "$BODY_FILE" ]
-#if body file exist - clear it
-then
-    echo "$HOSTNAME" > $BODY_FILE
-#if body file don't exist - make it
-else
-    touch $BODY_FILE
-    echo "$HOSTNAME" > $BODY_FILE
-fi
-
-# Prepare body - senders
-echo "Najwieksza wysyłka pochodzi od:" >> $BODY_FILE
-
-# List last 10 most valuable senders from server
-cat $MAIL_LOG | awk '{print $3}' | uniq -c | awk '{arr[$2]+=$1} END {for (i in arr) {print arr[i],i}}' | sort -n | tail | while read -r line
-do
-    if [[ $line =~ .*(cwd=.*) ]]
+# Prepare environment
+PREPARE_ENV () {
+    if [ -f "$BODY_FILE" ]
+    #if body file exist - clear it
     then
-    echo "$line" >> $BODY_FILE
+        echo "$HOSTNAME" > $BODY_FILE
+    #if body file don't exist - make it
+    else
+        touch $BODY_FILE
+        echo "$HOSTNAME" > $BODY_FILE
     fi
-done
 
-####### SEND #######
-mail -s "$SUBJECT" $MAIL_DESTINATION < $BODY_FILE
+    # Prepare body - senders
+    echo "Najwieksza wysyłka pochodzi od:" >> $BODY_FILE
+}
 
-##### CLEAR BODY #####
-rm -rf $BODY_FILE
+# Check the last 5 people sending the most messages. 
+CHECK_WHO () {
+    COMMAND=`cat $MAIL_LOG | awk '{print $3}' | uniq -c | awk '{arr[$2]+=$1} END {for (i in arr) {print arr[i],i}}' | sort -n | tail -$NUMBER_OF_PEOPLE`
+    echo "$COMMAND" | while read -r line
+    do
+        # Check if it's path to user
+        if [[ $line =~ .*(cwd=.*) ]]
+        then
+        echo "$line" >> $BODY_FILE
+        fi
+    done
+}
+
+SEND_MAIL () {
+    ####### SEND #######
+    mail -s "$SUBJECT" $MAIL_DESTINATION < $BODY_FILE
+
+    ##### CLEAR BODY #####
+    rm -rf $BODY_FILE
+}
+
+RUN () {
+    PREPARE_ENV
+    CHECK_WHO
+    SEND_MAIL
+}
+
+RUN

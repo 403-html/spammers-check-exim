e850b8c6727a8c4e81918f9303856c965a0969ab
Tymoteusz <tymoteusz.stepien@wp.pl>
2019-02-02 15:24:24 +0100

    Well escaping of functions

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# version 1.0.7
+# version 1.0.8
 # pre 1.1
 
 # Variables
@@ -56,11 +56,12 @@ CRON_CHECK () {
     CRON_SEARCH=`tail -n1 $EXIM_LOG | grep $USER | grep $exim_today_date | grep cwd | awk '{print $7}'`
 
     # Check if it's cron-job
-    if [[ $CRON_SEARCH =~ \-(FCronDaemon)$ ]]
+    if [[ "$CRON_SEARCH" =~ \-(FCronDaemon)$ ]]
     then
         WRITE_TO_FILE "$MAIL_USER wysyła maile najprawdopodobniej przez zadanie crona"
-        return 1
+        return
     fi
+    false
 }
 PASS_LEAK () {
     USER=$1
@@ -72,12 +73,13 @@ PASS_LEAK () {
     echo "$LOGGED_IP" | while read -r ip
     do
         IP_LOCATION=`csf -i $ip | awk -F "(" '{print $2}' | awk -F "/" '{print $1}'`
-        if ! [[ $IP_LOCATION -eq "PL" ]]
+        if ! [[ "$IP_LOCATION" =~ "PL" ]]
         then
             WRITE_TO_FILE "Dla $MAIL_USER prawdopodobnie nastąpił wyciek hasła do skrzynki pocztowej"
-            return 1
+            return
         fi
     done
+    false
 }
 FORM_SPAM () {
     USER=$1
@@ -85,36 +87,40 @@ FORM_SPAM () {
     echo "$USER"
     # Check if it's form spam
     SCRIPT_CHECK=`tail -n1 $EXIM_LOG | grep $USER | awk '{print $3}' | awk -F "/" '{print $4}'`
-    if [[ $SCRIPT_CHECK -eq "public_html" ]]
+    if [[ "$SCRIPT_CHECK" =~ "public_html" ]]
     then
         WRITE_TO_FILE "$MAIL_USER prawdopodobnie prowadzi wysyłkę z formularza/skrypu"
-        return 1
+        return
     fi
+    false
 }
 CHECK_FROM_WHERE () {
     AMOUNT=`echo $1 | awk -F' ' '{print $1}'`
     USER=`echo $1 | awk -F' ' '{print $2}'`
     
     ANSWER=$(CRON_CHECK "$USER")
-    if [[ $ANSWER -eq 1 ]]
+    if [[ $ANSWER ]]
     then
-        return 0
+        WRITE_TO_FILE "Ilość: $AMOUNT Miejsce wysyłki: $USER"
+        WRITE_TO_FILE ""
+        return
     fi
 
     ANSWER=$(PASS_LEAK "$USER")
-    if [[ $ANSWER -eq 1 ]]
+    if [[ $ANSWER ]]
     then
-        return 0
+        WRITE_TO_FILE "Ilość: $AMOUNT Miejsce wysyłki: $USER"
+        WRITE_TO_FILE ""
+        return
     fi
 
     ANSWER=$(FORM_SPAM "$USER")
-    if [[ $ANSWER -eq 1 ]]
+    if [[ $ANSWER ]]
     then
-        return 0
+        WRITE_TO_FILE "Ilość: $AMOUNT Miejsce wysyłki: $USER"
+        WRITE_TO_FILE ""
+        return
     fi
-
-    WRITE_TO_FILE "Ilość: $AMOUNT Miejsce wysyłki: $USER"
-    WRITE_TO_FILE ""
 }
 
 SEND_MAIL () {
